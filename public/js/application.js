fac0adcff3f3b6abc215f18b516a8cde9129cce0
cokeefe <chaemobile@gmail.com>
2013-08-20 20:22:40 -0700

    finish

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,20 @@
 $(document).ready(function() {
-  $('.awesomeness_teller').on('click', function() {
 
+  $('.awesomeness_teller').click(function(event){
+    var $target = $(event.target).text();
+    $('#awesomeness_holder').empty();
+    $('#awesomeness_holder').append($target);
   });
 
-  // What other events do you need to bind to make the other pages work?
+  $('.skill_teller').on('click', function(){
+    var $url_last = document.URL.split('/')[4];
+    $('#skill_holder').empty();
+    $('#skill_holder').append($url_last)
+  });
+
+  $('.meal_teller').on('click', function(){
+    $('#meal_holder').empty();
+    $('#meal_holder').append(meal);
+  });
+
 });
